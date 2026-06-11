4f758512bb6a521050874f49170432bca4c79470
rickc138-B <rick.c1388@gmail.com>
2026-06-11 14:59:14 +0000

    serve static HTML via express

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { NestFactory }  from '@nestjs/core';
-import { AppModule }    from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app  = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 3000;
-  app.enableCors();
-  await app.listen(port, '0.0.0.0');
-  console.log(`Course Assistant API running on port ${port}`);
+ const app = await NestFactory.create<NestExpressApplication>(AppModule);
+ const port = process.env.PORT ?? 3000;
+ app.enableCors();
+ app.useStaticAssets(join(__dirname, '..', 'public'));
+ await app.listen(port, '0.0.0.0');
+ console.log(`Course Assistant API running on port ${port}`);
 }
 bootstrap();
