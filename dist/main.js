8dca5a6b4240caa59716aeeacd314da41a68ec42
rickc138-B <rick.c1388@gmail.com>
2026-06-11 15:07:52 +0000

    fix API URL to Railway

--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const port = process.env.PORT ?? 3000;
     app.enableCors();
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
     await app.listen(port, '0.0.0.0');
     console.log(`Course Assistant API running on port ${port}`);
 }
